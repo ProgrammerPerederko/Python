496f65825bb58d78af3e35b03bc6d9989f6eb551
Daniel <perederkodaniel@gmail.com>
2026-07-26 12:54:53 -0300

    Terminando os exercicios das variaveis

--- Fundamentos/Variaveis/exercicios.py
+++ Fundamentos/Variaveis/exercicios.py
@@ -14,3 +14,21 @@ soma = idade + saldo
 
 # 5 imprima na tela o valor de soma
 print(soma)
+
+#-----------------------------------------------------------------|
+
+# 1 Crie uma Variavel chamada nota1 e atribua o valor 7.5 a ela
+# 2 Crie uma Variavel chamada nota2 e atribua o valor 8.3 a ela
+# 3 Crie uma Variavel chamada nota3 e atribua o valor 6.9 a ela
+# 4 Calcule a media das tres notas e atribua o resultado  a uma variavel chamada media.
+# 5 imprima o valor na tela formatado com duas casas decimais
+
+nota1,nota2,nota3 = 7.5,8.3,6.9
+
+media = (nota1 + nota2 + nota3) / 3
+
+#() (parênteses) têm prioridade na execução.
+
+#:.2f em uma f-string formata o número com duas casas decimais.
+
+print(f"Média: {media:.2f}")
