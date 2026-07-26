82038cddbb3f6ab39cbe00a28eb7dc0d833facef
Daniel <perederkodaniel@gmail.com>
2026-07-26 12:43:05 -0300

    Arrumando a estrutura das pastas e adicionadno exercicios

---
+++ Fundamentos/Variaveis/exercicios.py
@@ -0,0 +1,16 @@
+# Exercicios em Python para treinar variaveis:
+
+# 1 Declare uma variavel chamada idade e atribua o valor 25 a ela
+idade = 25
+
+# 2 Declare uma variavel chamada nome e atribua o valor João a ela
+nome = "João"
+
+# 3 Declare uma variavel chamada saldo e atribua o valor 100.50 a ela.
+saldo = 100.50
+
+# 4 Crie uma Variavel chamada soma e atribua a ela a soma das variaveis  idade e saldo.
+soma = idade + saldo
+
+# 5 imprima na tela o valor de soma
+print(soma)
