4fa71aaeffd6fd218af1ff739d3b9eedeef7e7a0
Daniel <perederkodaniel@gmail.com>
2026-07-26 00:13:37 -0300

    Variaveis Basicas em python

---
+++ Base/Variaveis.py
@@ -0,0 +1,24 @@
+# Tipos de Variaveis
+
+# Mutavel - Siginifica que podemos alterar o conteudo do objeto depois que ele foi criado.
+# Exemplo uma lista em python - Lista [1,5,6]
+
+# Imutavel - Significa que depois de declarada o conteudo do objeto, não pode ser alterado após sua criação.
+# Exemplo uma tupla em python -    
+
+# String (str) - Sequencia de caracteres que ocupam um espaço na memoria.
+String = "Daniel"
+
+# Inteiros (int) - Números inteiros, sem a parte decimal.
+Inteiro = 25
+
+# Ponto Flutuante (Float) - Números Reais, que tem uma parte decimal
+Flutuante = 24.0
+
+# Complexos (Complex) - Números complexos, que têm uma parte real e uma parte imaginaria
+Complexos = 4j + 2
+
+# Array (list) - Uma coleção ordenada e mutavel.
+Lista = [ 1,2,3]
+#
+Boolean = True
