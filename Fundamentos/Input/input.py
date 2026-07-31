721d6190d39acbba29fda929fe8ad1ab3a3a2068
Daniel <perederkodaniel@gmail.com>
2026-07-30 23:22:25 -0300

    Variaveis Notas em exemplos de inserção de notas

--- Fundamentos/Input/input.py
+++ Fundamentos/Input/input.py
@@ -2,3 +2,11 @@
 
 nome = input("Digite seu nome:")
 print("Seu nome é",nome)
+
+# Declaramos variaveis para armazenar os valores do input
+
+nota1,nota2,nota3 = input("Digite sua Nota1"),input("Digite sua Nota2"),input("Digite sua Nota3")
+
+print("Sua Nota1 é",nota1)
+print("Sua Nota2 é",nota2)
+print("Sua Nota3 é",nota3)
