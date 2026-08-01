e2303c91b7f840ced232f2a6b08707e8d7a23df9
Daniel <perederkodaniel@gmail.com>
2026-08-01 18:25:55 -0300

    Subindo o input da tabuada

--- Fundamentos/Input/input.py
+++ Fundamentos/Input/input.py
@@ -1,3 +1,12 @@
+"""
+Solicita ao usuário que insira seu nome e duas notas,calcula a média dessas notas,em seguida, 
+imprime uma mensagem contendo o nome do aluno e a média calculada.
+
+O resultado final combina os valores das variaveis, nome e media com as strings
+literais para criar a saida completa.
+
+"""
+
 # Treinando Input 
 
 nome = input("Digite seu nome:")
@@ -10,3 +19,11 @@ nota1,nota2,nota3 = input("Digite sua Nota1"),input("Digite sua Nota2"),input("D
 print("Sua Nota1 é",nota1)
 print("Sua Nota2 é",nota2)
 print("Sua Nota3 é",nota3)
+
+
+# Exercício: Tabuada Personalizada 
+
+tabuada_exercicio = int(input("\nInsira o número da tabuada que quer ver: "))
+
+for i in range(1, 11):
+    print(f"{tabuada_exercicio} x {i} = {tabuada_exercicio * i}")
