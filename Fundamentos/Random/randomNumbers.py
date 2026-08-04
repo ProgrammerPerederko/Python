c80b63fc5cce2597986d610344ca9c508edf901d
Daniel <daniels.perederko@gmail.com>
2026-08-04 16:00:09 -0300

    Corrigindo pasta Random

---
+++ Fundamentos/Random/randomNumbers.py
@@ -0,0 +1,14 @@
+# Biblioteca de Numeros randomicos
+
+import random
+
+print(random.randrange(1,100))
+
+# Gerar um numero de ponto flutuante aleatorio entre 0 e 1
+
+print(random.random())
+
+# Gerar um numero inteiro aleatorio entre dois valores:
+
+print(random.randint(10, 20))
+
