d5d0afb791b297b88cd448e27e4b486ca6669e22
Daniel <daniels.perederko@gmail.com>
2026-08-06 21:13:42 -0300

    Inserindo testes de posicionamenton de strings

---
+++ Fundamentos/strings/strings.py
@@ -0,0 +1,11 @@
+# Posicionamento de cada string.
+
+# Imprimindo a pósição de cada letra 
+posicao_letra = "PYTHON"
+
+print(posicao_letra[0])
+print(posicao_letra[1])
+print(posicao_letra[2])
+print(posicao_letra[3])
+print(posicao_letra[4])
+print(posicao_letra[5])
